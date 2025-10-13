04221138da7bcc99f911d714dd9434f97a78a954
m0ftah <moftah0550@gmail.com>
2025-10-13 11:40:13 +0200

    css issue

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -14,13 +14,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Css Styles -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/elegant-icons.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/slicknav.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/filament/bootstrap.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/filament/font-awesome.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/filament/elegant-icons.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/filament/owl.carousel.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/filament/magnific-popup.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/filament/slicknav.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/filament/style.css') }}" type="text/css">
 
     @stack('styles')
 </head>
